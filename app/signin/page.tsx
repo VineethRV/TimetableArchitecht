48ac4a3c2aa06f0788d764d23ab4705df2c20d12
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-10-27 14:57:18 +0530

    signin page done

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import Header from '../components/SigninPage/Header'
-import SigninFormCard from '../components/SigninPage/SigninFormCard'
+import React from 'react';
+import Header from '../components/SigninPage/Header';
+import SigninFormCard from '../components/SigninPage/SigninFormCard';
+import SignInIllus1 from '@/public/Illustrations/Sign1.png';
+import SignInIllus2 from '@/public/Illustrations/Sign2.png';
+import Image from 'next/image';
 
 const Page = () => {
   return (
     <>
-        <Header/>
-        <div className='flex justify-center items-center h-[86vh]'>
-        <SigninFormCard/>
+      <div className="relative h-screen w-screen overflow-hidden bg-gray-50">
+        <Image
+          className="absolute w-[370px] h-[370px] bottom-[-50px] left-[-50px] opacity-80"
+          src={SignInIllus1}
+          alt="Signin1"
+        />
+        <Image
+          className="absolute w-[370px] h-[370px] bottom-[-100px] right-[-50px] opacity-80"
+          src={SignInIllus2}
+          alt="Signin2"
+        />
+        <Header />
+        <div className="flex justify-center items-center h-[86vh]">
+          <SigninFormCard />
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
