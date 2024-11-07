6cddf5d4c47945418a44b5c6de2c2d122f8f2e49
Shreya Prasad <shreyaprasad019@gmail.com>
2024-11-08 00:40:28 +0530

    requested review changed made

--- app/dashboard/teacher/Add/page.tsx
+++ app/dashboard/teacher/Add/page.tsx
@@ -1,9 +1,9 @@
-'use client';
-import React from 'react';
-import { InfoCircleOutlined, UploadOutlined } from '@ant-design/icons';
-import { Button, message, Form, Input, Select, Tooltip, Upload } from 'antd';
-import Timetable from '@/app/components/timetable';
-import { motion } from 'framer-motion'
+"use client";
+import React from "react";
+import { InfoCircleOutlined, UploadOutlined } from "@ant-design/icons";
+import { Button, message, Form, Input, Select, Tooltip, Upload } from "antd";
+import Timetable from "@/app/components/timetable";
+import { motion } from "framer-motion";
 const formItemLayout = {
   labelCol: {
     xs: { span: 24 },
@@ -18,15 +18,21 @@ const formItemLayout = {
 const AddTeacherpage: React.FC = () => {
   const [form] = Form.useForm();
   const success = () => {
-      message.success('Teacher Added successfully!', 5);
+    message.success("Teacher Added successfully!", 5);
   };
 
   return (
     <div className="font-inter text-[18px] leading-[28px] font-bold text-[#171A1F] ml-5 mt-5">
-      <div className='flex justify-between text-[#636AE8FF] font-inter text-[18px] leading-[28px] text-bold mr-8'>&#8592; Back
-      <Upload>
-        <Button icon={<UploadOutlined /> } className="text-[#636AE8FF] border-[#636AE8FF] ">Import</Button>
-      </Upload>
+      <div className="flex justify-between text-[#636AE8FF] font-inter text-[18px] leading-[28px] text-bold mr-8">
+        &#8592; Back
+        <Upload>
+          <Button
+            icon={<UploadOutlined />}
+            className="text-[#636AE8FF] border-[#636AE8FF] "
+          >
+            Import
+          </Button>
+        </Upload>
       </div>
       <motion.div
         initial={{ opacity: 0, y: -20 }}
@@ -34,45 +40,50 @@ const AddTeacherpage: React.FC = () => {
         transition={{
           duration: 0.5,
           ease: "easeInOut",
-        }} className="flex justify-left items-center h-[86vh] mt-[40vh] ml-4 ">
-      <Form
-        {...formItemLayout}
-        form={form}
-        layout="vertical"
-        requiredMark >
-        <Form.Item label="Teacher Name" required>
-          <Input placeholder="Name" className="font-inter font-normal"
-          />
-        </Form.Item>
-        <Form.Item label="Initials" required>
-          <Input placeholder="Initials" className='font-inter font-normal'/>
-        </Form.Item>
-        <Form.Item label="Email Id" required>
-          <Input placeholder="Email Id" className='font-inter font-normal' />
-        </Form.Item>
-        <Form.Item
-        label="Department"
-        name="Select">
-        <Select className='font-inter font-normal' />
-      </Form.Item>
-      <label>
-          <span>Schedule</span>
-          <Tooltip title="Tooltip with customize icon" icon={<InfoCircleOutlined />}>
-            <InfoCircleOutlined className="ml-2 text-[#636AE8FF]" />
-          </Tooltip>
-      </label>
-      <div className="flex justify-left" >
-      <Timetable />
-      </div>
-      <div className="flex justify-end w-[55vm]">
-        <Form.Item>
-          <Button className="border-[#636AE8FF] text-[#636AE8FF] mr-[30px] w-[75px] h-[32px]">Clear</Button>
-        </Form.Item>
-        <Form.Item>
-          <Button onClick={success} className="bg-[#636AE8FF] text-[#FFFFFF] w-[75px] h-[32px]">Submit</Button>
-        </Form.Item>
-      </div>
-      </Form>
+        }}
+        className="flex justify-left items-center h-[86vh] mt-[40vh] ml-4 "
+      >
+        <Form {...formItemLayout} form={form} layout="vertical" requiredMark>
+          <Form.Item label="Teacher Name" required>
+            <Input placeholder="Name" className="font-inter font-normal" />
+          </Form.Item>
+          <Form.Item label="Initials" required>
+            <Input placeholder="Initials" className="font-inter font-normal" />
+          </Form.Item>
+          <Form.Item label="Email Id" required>
+            <Input placeholder="Email Id" className="font-inter font-normal" />
+          </Form.Item>
+          <Form.Item label="Department" name="Select">
+            <Select className="font-inter font-normal" />
+          </Form.Item>
+          <label>
+            <span>Schedule</span>
+            <Tooltip
+              title="Tooltip with customize icon"
+              icon={<InfoCircleOutlined />}
+            >
+              <InfoCircleOutlined className="ml-2 text-[#636AE8FF]" />
+            </Tooltip>
+          </label>
+          <div className="flex justify-left">
+            <Timetable />
+          </div>
+          <div className="flex justify-end w-[55vm]">
+            <Form.Item>
+              <Button className="border-[#636AE8FF] text-[#636AE8FF] mr-[30px] w-[75px] h-[32px]">
+                Clear
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                onClick={success}
+                className="bg-[#636AE8FF] text-[#FFFFFF] w-[75px] h-[32px]"
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
       </motion.div>
     </div>
   );
--- app/dashboard/teacher/layout.tsx
+++ app/dashboard/teacher/layout.tsx
@@ -1,74 +1,114 @@
 "use client";
 
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Layout, Divider, Button } from "antd";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar, faUserPen, faUserPlus, faChalkboardTeacher, faClockRotateLeft } from '@fortawesome/free-solid-svg-icons';
-import { useRouter } from 'next/navigation';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faUserPen,
+  faUserPlus,
+  faChalkboardTeacher,
+  faClockRotateLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/navigation";
 
-const { Header, Content ,Sider} = Layout;
+const { Header, Content, Sider } = Layout;
 
 const Sidebar: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const Router = useRouter();
   const [selected, setSelected] = useState("Modify Attributes");
   const menuItems = [
-    { label: "Add a Teacher", icon: faUserPlus, url: "/Add" },
+    { label: "Add a Teacher", icon: faUserPlus, url: "/add" },
     { label: "Modify Attributes", icon: faUserPen, url: "" },
     { label: "Rank Timewise", icon: faClockRotateLeft, url: "/Rank-timetable" },
     {
-      label: "Timeslot Dependent",icon: faCalendar,url: "/Timeslot-dependent",
+      label: "Timeslot Dependent",
+      icon: faCalendar,
+      url: "/Timeslot-dependent",
     },
   ];
 
   useEffect(() => {
-    const storedKey = localStorage.getItem('selected');
-    if (storedKey) { 
-      setSelected(storedKey);}},[]);
-      const handleClick = (label: string, url: string) => {
-        setSelected(label); 
-        localStorage.setItem('selected', label); 
-        Router.push(`/dashboard/teacher${url}`);
-      };
+    const storedKey = localStorage.getItem("selected");
+    if (storedKey) {
+      setSelected(storedKey);
+    }
+  }, []);
+  const handleClick = (label: string, url: string) => {
+    setSelected(label);
+    localStorage.setItem("selected", label);
+    Router.push(`/dashboard/teacher${url}`);
+  };
 
   return (
-    <Layout className='min-h-screen'>
+    <Layout className="min-h-screen">
       <Sider className="h-screen bg-white ">
         <div className="flex justify-left pt-[20px] pl-[20px] h-[7vh] ">
-          <FontAwesomeIcon icon={faChalkboardTeacher} className='w-[30px] h-[40px]' />
-          <span className=' pl-2 text-[18px] font-semibold text-[#171A1FFF]' style={{fontFamily:'Archivo'}}>Teachers</span> 
+          <FontAwesomeIcon
+            icon={faChalkboardTeacher}
+            className="w-[30px] h-[40px]"
+          />
+          <span
+            className=" pl-2 text-[18px] font-semibold text-[#171A1FFF]"
+            style={{ fontFamily: "Archivo" }}
+          >
+            Teachers
+          </span>
         </div>
         <Divider />
-        <div className='pl-[10px] font-[14px] h-[25vh] flex flex-col justify-left' style={{fontFamily:'Inter'}} >
+        <div
+          className="pl-[10px] font-[14px] h-[25vh] flex flex-col justify-left"
+          style={{ fontFamily: "Inter" }}
+        >
           {menuItems.slice(0, 2).map((item) => (
             <div
-            className={`relative flex items-center justify-start pt-5 pl-5 text-[#565E6C] font-normal ${ selected === item.label ? 'item-selected' : '' }`}
+              className={`relative flex items-center justify-start pt-5 pl-5 text-[#565E6C] font-normal ${
+                selected === item.label ? "item-selected" : ""
+              }`}
               key={item.label}
-              onClick={() => handleClick(item.label, item.url)}>
-              <FontAwesomeIcon icon={item.icon} className='w-[20px] h-[30px]' />
-              <span className='pl-[10px]'>{item.label}</span>
+              onClick={() => handleClick(item.label, item.url)}
+            >
+              <FontAwesomeIcon icon={item.icon} className="w-[20px] h-[30px]" />
+              <span className="pl-[10px]">{item.label}</span>
             </div>
           ))}
         </div>
         <Divider />
-        <div className='pl-[10px] font-[14px]  h-[25vh] flex flex-col justify-left' style={{fontFamily:'Inter'}}>
+        <div
+          className="pl-[10px] font-[14px]  h-[25vh] flex flex-col justify-left"
+          style={{ fontFamily: "Inter" }}
+        >
           {menuItems.slice(2, 4).map((item) => (
             <div
-            className={`relative flex items-center justify-start pt-5 pl-5 text-[#565E6C] font-normal ${ selected === item.label ? 'item-selected' : ''}`}
+              className={`relative flex items-center justify-start pt-5 pl-5 text-[#565E6C] font-normal ${
+                selected === item.label ? "item-selected" : ""
+              }`}
               key={item.label}
-              onClick={() => setSelected(item.label)}>
-              <FontAwesomeIcon icon={item.icon} className='w-[20px] h-[30px]' />
-              <span className='pl-[10px]'>{item.label}</span>
+              onClick={() => setSelected(item.label)}
+            >
+              <FontAwesomeIcon icon={item.icon} className="w-[20px] h-[30px]" />
+              <span className="pl-[10px]">{item.label}</span>
             </div>
           ))}
-            <Button
-              onClick={() => Router.push(`/teacher/Consolidated`)} className='flex justify-center  mt-[25px] bg-[#636AE8FF] text-[#FFFFFF]'>
-              Generate Consolidated 
-            </Button>
+          <Button
+            onClick={() => Router.push(`/teacher/Consolidated`)}
+            className="flex justify-center  mt-[25px] bg-[#636AE8FF] text-[#FFFFFF]"
+          >
+            Generate Consolidated
+          </Button>
         </div>
       </Sider>
-      <Layout >
-      <Header className="fixed h-[10vh] bg-white text-[#636AE8FF] text-2xl leading-[48px] font-extrabold pt-2" style={{ fontFamily:'Archivo', width: 'calc(100% - 20vw)' }}>Teachers</Header>
-        <Content className="fixed  top-[10vh] overflow-y-auto h-[90vh] p-4 bg-white " style={{ width: 'calc(100% - 20vw )' }} >
+      <Layout>
+        <Header
+          className="fixed h-[10vh] bg-white text-[#636AE8FF] text-2xl leading-[48px] font-extrabold pt-2"
+          style={{ fontFamily: "Archivo", width: "calc(100% - 20vw)" }}
+        >
+          Teachers
+        </Header>
+        <Content
+          className="fixed  top-[10vh] overflow-y-auto h-[90vh] p-4 bg-white "
+          style={{ width: "calc(100% - 20vw )" }}
+        >
           {children}
         </Content>
       </Layout>
