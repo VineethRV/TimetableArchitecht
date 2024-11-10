73a5e66ababb4fcf9dad3a40fb84af03adb4638c
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-11-10 17:33:12 +0530

    vercel fix

--- app/dashboard/rooms/add/page.tsx
+++ app/dashboard/rooms/add/page.tsx
@@ -1,7 +1,7 @@
 'use client';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { InfoCircleOutlined, UploadOutlined } from '@ant-design/icons';
-import { Button, message, Form, Input, Select, Tooltip, Upload,InputNumber,Radio } from 'antd';
+import { Button, message, Form, Input, Select, Tooltip, Upload, InputNumber, Radio } from 'antd';
 import type { RadioChangeEvent } from 'antd';
 import Timetable from '@/app/components/timetable';
 import { motion } from 'framer-motion'
@@ -19,7 +19,7 @@ const formItemLayout = {
 const AddRoomForm: React.FC = () => {
   const [form] = Form.useForm();
   const success = () => {
-      message.success('Teacher Added successfully!', 5);
+    message.success('Teacher Added successfully!', 5);
   };
 
   const [value, setValue] = useState(1);
@@ -33,9 +33,9 @@ const AddRoomForm: React.FC = () => {
   return (
     <div className="font-inter text-[18px] leading-[28px] font-bold text-[#171A1F] ml-5 mt-5">
       <div className='flex justify-between text-[#636AE8FF] font-inter text-[18px] leading-[28px] text-bold mr-8'>&#8592; Back
-      <Upload>
-        <Button icon={<UploadOutlined /> } className="text-[#636AE8FF] border-[#636AE8FF] ">Import</Button>
-      </Upload>
+        <Upload>
+          <Button icon={<UploadOutlined />} className="text-[#636AE8FF] border-[#636AE8FF] ">Import</Button>
+        </Upload>
       </div>
       <motion.div
         initial={{ opacity: 0, y: -20 }}
@@ -44,47 +44,47 @@ const AddRoomForm: React.FC = () => {
           duration: 0.5,
           ease: "easeInOut",
         }} className="flex justify-left items-center h-[86vh] mt-[40vh] ml-4 ">
-      <Form
-        {...formItemLayout}
-        form={form}
-        layout="vertical"
-        requiredMark >
-        <Form.Item label="ClassRoom Name" required>
-          <Input placeholder="Name" className="font-inter font-normal"
-          />
-        </Form.Item>
-        <Form.Item label="ClassRoom Capacity" required>
-        <InputNumber min={0} placeholder="Capacity" className='w-full font-inter font-normal' />
-        </Form.Item>
-        <Form.Item label="Is it a Lab?" required>
-              <Radio.Group onChange={onChange} value={value}>
-            <Radio value={1} className='mr-10'>Yes</Radio>
-            <Radio value={2}  className='ml-10 color-[#636AE8FF]'>No</Radio>
-              </Radio.Group>
-        </Form.Item>
-        <Form.Item
-        label="Department"
-        name="Select">
-        <Select className='font-inter font-normal' />
-      </Form.Item>
-      <label>
-          <span>Schedule</span>
-          <Tooltip title="Tooltip with customize icon" icon={<InfoCircleOutlined />}>
-            <InfoCircleOutlined className="ml-2 text-[#636AE8FF]" />
-          </Tooltip>
-      </label>
-      <div className="flex justify-left" >
-      <Timetable />
-      </div>
-      <div className="flex justify-end w-[55vm]">
-        <Form.Item>
-          <Button className="border-[#636AE8FF] text-[#636AE8FF] mr-[30px] w-[75px] h-[32px]">Clear</Button>
-        </Form.Item>
-        <Form.Item>
-          <Button onClick={success} className="bg-[#636AE8FF] text-[#FFFFFF] w-[75px] h-[32px]">Submit</Button>
-        </Form.Item>
-      </div>
-      </Form>
+        <Form
+          {...formItemLayout}
+          form={form}
+          layout="vertical"
+          requiredMark >
+          <Form.Item label="ClassRoom Name" required>
+            <Input placeholder="Name" className="font-inter font-normal"
+            />
+          </Form.Item>
+          <Form.Item label="ClassRoom Capacity" required>
+            <InputNumber min={0} placeholder="Capacity" className='w-full font-inter font-normal' />
+          </Form.Item>
+          <Form.Item label="Is it a Lab?" required>
+            <Radio.Group onChange={onChange} value={value}>
+              <Radio value={1} className='mr-10'>Yes</Radio>
+              <Radio value={2} className='ml-10 color-[#636AE8FF]'>No</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            label="Department"
+            name="Select">
+            <Select className='font-inter font-normal' />
+          </Form.Item>
+          <label>
+            <span>Schedule</span>
+            <Tooltip title="Tooltip with customize icon">
+              <InfoCircleOutlined className="ml-2 text-[#636AE8FF]" />
+            </Tooltip>
+          </label>
+          <div className="flex justify-left" >
+            <Timetable />
+          </div>
+          <div className="flex justify-end w-[55vm]">
+            <Form.Item>
+              <Button className="border-[#636AE8FF] text-[#636AE8FF] mr-[30px] w-[75px] h-[32px]">Clear</Button>
+            </Form.Item>
+            <Form.Item>
+              <Button onClick={success} className="bg-[#636AE8FF] text-[#FFFFFF] w-[75px] h-[32px]">Submit</Button>
+            </Form.Item>
+          </div>
+        </Form>
       </motion.div>
     </div>
   );
