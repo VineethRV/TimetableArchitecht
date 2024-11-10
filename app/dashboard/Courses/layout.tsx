2f10e04be71191621e5a36f2488749fa7ba87027
Shreya Prasad <shreyaprasad019@gmail.com>
2024-11-10 16:13:04 +0530

    fixed import error

--- app/dashboard/Courses/layout.tsx
+++ app/dashboard/Courses/layout.tsx
@@ -3,14 +3,13 @@
 import React, { useState } from "react";
 import { Layout, Divider } from "antd";
 const { Sider } = Layout;
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBasketShopping,
-  faFlask,
-  faBook,
-  faCirclePlus,
-  faPenToSquare,
-} from "@fortawesome/free-solid-svg-icons";
+  FaBasketShopping,
+  FaFlask,
+  FaBook,
+  FaCirclePlus,
+  FaPenToSquare,
+} from "react-icons/fa6"
 import { useRouter, usePathname } from "next/navigation";
 
 const Sidebar: React.FC = () => {
@@ -30,21 +29,21 @@ const Sidebar: React.FC = () => {
   };
 
   const menuItems1 = [
-    { label: "Electives", icon: faBasketShopping, url: "/subjects/Electives" },
-    { label: "Labs", icon: faFlask, url: "/subjects/Labs" },
-    { label: "Core Courses", icon: faBook, url: "/subjects/CoreCourses" },
+    { label: "Electives", icon: FaBasketShopping, url: "/subjects/Electives" },
+    { label: "Labs", icon: FaFlask, url: "/subjects/Labs" },
+    { label: "Core Courses", icon: FaBook, url: "/subjects/CoreCourses" },
   ];
 
   const menuItems2 = [
-    { label: "Add a Course", icon: faCirclePlus, url: "/add" },
-    { label: "Modify Course", icon: faPenToSquare, url: "" },
+    { label: "Add a Course", icon: FaCirclePlus, url: "/add" },
+    { label: "Modify Course", icon: FaPenToSquare, url: "" },
   ];
 
   return (
     <Layout className="min-h-screen">
       <Sider className="fixed left-[5vw] top-0 w-[17vw] h-screen bg-white z-[100]">
         <div className="flex justify-left text-black-bold items-center pt-[20px] pl-[20px] h-[7vh] ">
-          <FontAwesomeIcon icon={faBook} className="w-[20px] h-[25px]" />
+          <FaBook className="w-[20px] h-[25px]" />
           <span
             className=" pl-2 text-[18px] font-semibold text-[#171A1FFF]"
             style={{ fontFamily: "Archivo" }}
@@ -57,16 +56,16 @@ const Sidebar: React.FC = () => {
           className="pl-[10px] font-[14px] font-family-inter h-[25vh] flex flex-col justify-left"
           style={{ fontFamily: "Inter" }}
         >
-          {menuItems1.map((item) => (
+          {menuItems1.map(({label,icon:Icon,url}) => (
             <div
               className={`relative flex items-center justify-start pt-5 pl-5 cursor-pointer text-[#565E6C] font-normal ${
-                selected1 === item.label ? "item-selected" : ""
+                selected1 === label ? "item-selected" : ""
               }`}
-              key={item.label}
-              onClick={() => handleClick1(item.label, item.url)}
+              key={label}
+              onClick={() => handleClick1(label, url)}
             >
-              <FontAwesomeIcon icon={item.icon} className="w-[20px] h-[30px]" />
-              <span className="pl-[10px]">{item.label}</span>
+              <Icon className="w-[20px] h-[30px]" />
+              <span className="pl-[10px]">{label}</span>
             </div>
           ))}
         </div>
@@ -75,16 +74,16 @@ const Sidebar: React.FC = () => {
           className="pl-[10px] font-[14px] font-family-inter h-[25vh] flex flex-col justify-left"
           style={{ fontFamily: "Inter" }}
         >
-          {menuItems2.map((item) => (
+          {menuItems2.map(({label,icon:Icon,url}) => (
             <div
               className={`relative flex items-center justify-start pt-5 pl-5 cursor-pointer text-[#565E6C] font-normal ${
-                selected2 === item.label ? "item-selected" : ""
+                selected2 === label ? "item-selected" : ""
               }`}
-              key={item.label}
-              onClick={() => handleClick2(item.label, item.url)}
+              key={label}
+              onClick={() => handleClick2(label, url)}
             >
-              <FontAwesomeIcon icon={item.icon} className="w-[20px] h-[30px]" />
-              <span className="pl-[10px]">{item.label}</span>
+              <Icon className="w-[20px] h-[30px]" />
+              <span className="pl-[10px]">{label}</span>
             </div>
           ))}
         </div>
