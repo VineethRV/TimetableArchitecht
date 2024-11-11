3bce014809d55ab597ae799fd177a3b8f2939af3
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-11-11 08:53:02 +0530

    changed auth structure

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import PrismaClientManager from '../pgConnect';
 import bcrypt from 'bcrypt'
+import { statusCodes } from '@/app/types/statusCodes';
 
 const secretKey = process.env.JWT_SECRET_KEY || ""
 const prisma = PrismaClientManager.getInstance().getPrismaClient();
@@ -15,48 +16,78 @@ export const checkAuthentication = async (token: string): Promise<boolean> => {
   }
 };
 
-export const login = async (email: string, pass: string): Promise<string | null> => {
+export const login = async (email: string, pass: string): Promise<{ status: number, token: string }> => {
 
-  const user = await prisma.user.findFirst({
-    where: {
-      email
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email
+      }
+    })
+
+
+    if (!user) {
+      return {
+        status: statusCodes.NOT_FOUND,
+        token: ""
+      }
     }
-  })
 
-  if (!user) {
-    throw new Error("User not found")
+    const validatePass = await bcrypt.compare(pass, user?.hashedPass || "");
+
+    if (!validatePass) {
+      return {
+        status: statusCodes.UNAUTHORIZED,
+        token: ""
+      }
+    }
+
+    const token = jwt.sign({
+      email: user?.email
+    }, secretKey);
+
+    return {
+      status: statusCodes.OK,
+      token
+    }
+  }
+  catch (e) {
+    return {
+      status: statusCodes.INTERNAL_SERVER_ERROR,
+      token: ""
+    }
   }
 
-  const validatePass = await bcrypt.compare(pass, user?.hashedPass || "");
-
-  if (!validatePass) {
-    throw new Error("Invalid credentials");
-  }
-
-  const token = jwt.sign({
-    email: user?.email
-  }, secretKey);
-
-  return token;
 };
 
-export const register = async (name: string, email: string, password: string) => {
+export const register = async (name: string, email: string, password: string): Promise<{ status: number, token: string }> => {
   // check if fields are following proper rules
 
-  const hashedPass = await bcrypt.hash(password, 10);
+  try {
+    const hashedPass = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        hashedPass,
+        hasAccess: false
+      }
+    })
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      hashedPass,
-      hasAccess: false
+    const token = jwt.sign({
+      email
+    }, secretKey);
+
+    return {
+      status: statusCodes.OK,
+      token
     }
-  })
 
-  const token = jwt.sign({
-    email
-  }, secretKey);
-
-  return token;
+  }
+  catch (e) {
+    return {
+      status: statusCodes.INTERNAL_SERVER_ERROR,
+      token: ""
+    }
+  }
 }
