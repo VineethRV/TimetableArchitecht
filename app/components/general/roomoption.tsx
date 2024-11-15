5385ab01902ddc9b472fb6471fa3960da3d10380
Shreya Prasad <shreyaprasad019@gmail.com>
2024-11-15 11:49:15 +0530

    Check

--- app/components/general/roomoption.tsx
+++ app/components/general/roomoption.tsx
@@ -1,60 +1,9 @@
-import React from 'react';
-import { Cascader, Tag, CascaderProps } from 'antd';
 
 const RoomOptions: React.FC = () => {
-  const tagRender = (props: CascaderProps['tagRender']) => {
-    const { label, closable, onClose } = props;
 
     return (
-      <Tag
-        color="#F2F2FDFF"
-        closable={closable}
-        onClose={onClose}
-        style={{ borderRadius: '12px', color: '#636AE8FF' }}
-        closeIcon={<span style={{ color: 'red' }}>×</span>}
-      >
-        {label}
-      </Tag>
+    <div>Hello</div>  
     );
-  };
-
-  const options = [
-    {
-      label: 'CSE',
-      value: 'CSE',
-      children: [
-        { label: 'CSE101', value: 'CSE101' },
-        { label: 'CSE102', value: 'CSE102' },
-        { label: 'CSE103', value: 'CSE103' },
-      ],
-    },
-    {
-      label: 'ISE',
-      value: 'ISE',
-      children: [
-        { label: 'ISE101', value: 'ISE101' },
-        { label: 'ISE102', value: 'ISE102' },
-        { label: 'ISE103', value: 'ISE103' },
-      ],
-    },
-  ];
-
-  return (
-    <div>
-      <Cascader
-        options={options}
-        multiple
-        maxTagCount="responsive"
-        tagRender={tagRender}
-        showCheckedStrategy={Cascader.SHOW_CHILD}
-        placeholder="Default Classrooms"
-        style={{
-          width: '100%',
-          fontWeight: '400',
-        }}
-      />
-    </div>
-  );
 };
 
 export default RoomOptions;
